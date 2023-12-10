4be30c22d7e6683be87e70076288d81cccf4746f
jgzalez <kingdd2367@gmail.com>
2023-12-10 15:30:30 -0400

    Registro y Wiki (Establecimientos)

--- lib/NavPages/EstablismentReg.dart
+++ lib/NavPages/EstablismentReg.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
 
 class EstablishmentRegistrationForm extends StatefulWidget {
   @override
@@ -9,18 +10,48 @@ class EstablishmentRegistrationForm extends StatefulWidget {
 class _EstablishmentRegistrationFormState
     extends State<EstablishmentRegistrationForm> {
   final _formKey = GlobalKey<FormState>();
+
   // Controladores de texto
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _locationController = TextEditingController();
   final TextEditingController _employeeCountController =
       TextEditingController();
-  final TextEditingController _locationController = TextEditingController();
-  // ... otros controladores para cada campo
+  final TextEditingController _operatingHoursController =
+      TextEditingController();
+  final TextEditingController _sizeController = TextEditingController();
+  final TextEditingController _customerFlowController = TextEditingController();
+  final TextEditingController _establishmentTypeController =
+      TextEditingController();
+  final TextEditingController _maxCapacityController = TextEditingController();
+  final TextEditingController _foundationYearController =
+      TextEditingController();
+  // Considera usar controladores adicionales para archivos y otros campos
+
+  // Variables para guardar los archivos seleccionados
+  Map<String, PlatformFile> _selectedFiles = {};
 
   @override
   void dispose() {
-    _employeeCountController.dispose();
+    _nameController.dispose();
     _locationController.dispose();
-    // ... dispose otros controladores
+    _employeeCountController.dispose();
+    _operatingHoursController.dispose();
+    _sizeController.dispose();
+    _customerFlowController.dispose();
+    _establishmentTypeController.dispose();
+    _maxCapacityController.dispose();
+    _foundationYearController.dispose();
+    // Dispose otros controladores
     super.dispose();
+  }
+
+  Future<void> _pickFile(String key) async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if (result != null) {
+      setState(() {
+        _selectedFiles[key] = result.files.first;
+      });
+    }
   }
 
   @override
@@ -34,36 +65,35 @@ class _EstablishmentRegistrationFormState
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              TextFormField(
-                controller: _employeeCountController,
-                decoration: InputDecoration(labelText: 'Cantidad de Empleados'),
-                keyboardType: TextInputType.number,
-                // Valida el campo
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese la cantidad de empleados';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _locationController,
-                decoration: InputDecoration(labelText: 'Ubicación'),
-                // Valida el campo
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese la ubicación';
-                  }
-                  return null;
-                },
-              ),
-              // ... otros TextFormField para cada dato ...
+              buildTextFormField(_nameController, 'Nombre del Establecimiento'),
+              buildTextFormField(_locationController, 'Ubicación Geográfica'),
+              buildTextFormField(
+                  _employeeCountController, 'Número de Empleados',
+                  isNumeric: true),
+              buildTextFormField(
+                  _operatingHoursController, 'Horarios de Atención'),
+              buildTextFormField(_sizeController, 'Tamaño del Establecimiento',
+                  isNumeric: true),
+              buildTextFormField(_customerFlowController, 'Flujo de Clientes',
+                  isNumeric: true),
+              buildTextFormField(
+                  _establishmentTypeController, 'Tipo de Establecimiento'),
+              buildTextFormField(_maxCapacityController, 'Aforo Máximo',
+                  isNumeric: true),
+              buildTextFormField(_foundationYearController, 'Año de Fundación',
+                  isNumeric: true),
+
+              // Campos para la selección de archivos
+              filePickerField('Valoraciones de Clientes', 'customerRatings'),
+              filePickerField('Número de Reseñas', 'reviewCount'),
+              //... otros campos para archivos aquí
+
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 16.0),
                 child: ElevatedButton(
                   onPressed: () {
                     if (_formKey.currentState!.validate()) {
-                      // Procesa los datos del formulario
+                      // Procesa los datos del formulario junto con los archivos seleccionados
                     }
                   },
                   child: Text('Registrar'),
@@ -73,6 +103,37 @@ class _EstablishmentRegistrationFormState
           ),
         ),
       ),
+    );
+  }
+
+  Widget buildTextFormField(TextEditingController controller, String label,
+      {bool isNumeric = false}) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(labelText: label),
+      keyboardType: isNumeric ? TextInputType.number : TextInputType.text,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Por favor ingrese $label';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget filePickerField(String label, String key) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(label, style: TextStyle(fontWeight: FontWeight.bold)),
+        SizedBox(height: 8),
+        ElevatedButton(
+          onPressed: () => _pickFile(key),
+          child: Text('Seleccionar Archivo'),
+        ),
+        SizedBox(height: 8),
+        Text(_selectedFiles[key]?.name ?? 'Ningún archivo seleccionado'),
+      ],
     );
   }
 }
--- lib/Wiki/InstitutionsWiki.dart
+++ lib/Wiki/InstitutionsWiki.dart
@@ -20,43 +20,67 @@ class InstitutionsWiki extends StatelessWidget {
               // Descripción General
               sectionTitle('Descripción General'),
               sectionContent(
-                  'Los establecimientos del Grupo Ramos en la aplicación se dividen en dos categorías: '
-                  'Sirena Market e Híper Market. Cada categoría tiene modelos predictivos asignados para evaluar y analizar su rendimiento.'
-                  ' \n Los mismos estarán representados en la intefaz de establecimientos como tarjetas'
-                  'Para registrar un nuevo establecimiento, el usuario debe presionar la inter'),
+                  'Esta sección del aplicativo está destinada a la administración y creación de los diferentes establecimientos a ser procesados y utilizados por el aplicativo \n \n'
+                  'Los establecimientos del Grupo Ramos en la aplicación se dividen en dos categorías: \n \n'
+                  'Sirena Market e Híper Market. \n\n'
+                  'Cada categoría tiene modelos predictivos asignados para evaluar y analizar su rendimiento.'
+                  '\nLos mismos estarán representados en la intefaz de establecimientos como tarjetas con algunas informaciones relevantes\n\n'
+                  'Para registrar un nuevo establecimiento, el usuario debe presionar el botón superior del modal  "Crear Nuevo"\n'),
 
               // Estructura de Datos y Modelos
               sectionTitle('Estructura de Datos y Modelos'),
               sectionContent(
-                  'Los usuarios deben introducir información detallada sobre cada establecimiento, incluyendo nombre, ubicación, '
-                  'tipo, horarios de atención, entre otros. Estos datos son fundamentales para el análisis predictivo en la aplicación.'),
+                  'Al registrar un determinado establecimiento, los usuarios del aplicativo deben introducir información detallada sobre el mismo.\n'),
+              sectionSubtitle('Modelo de Entidad:'),
+              sectionContent(
+                  'El modelo de establecimiento cuenta de los siguientes atributos: \n\n'
+                  '1- Nombre del Establecimiento\n'
+                  '2- Ubicación Geográfica\n'
+                  '3- Número de Empleados\n'
+                  '4- Horarios de Atención\n'
+                  '5- Tamaño del Establecimiento\n'
+                  '6- Flujo de Clientes\n'
+                  '7- Tipo de Establecimiento\n'
+                  '8- Aforo Máximo\n'
+                  '9- Año de Fundación\n'
+                  '10- Valoraciones de Clientes (Archivo)\n'
+                  '11- Número de Reseñas (Archivo)\n'
+                  '12- Historial de Ventas (Archivo)\n'
+                  '13- Datos Demográficos de Clientes (Archivo)\n'
+                  '14- Ingresos Anuales (Archivo)\n'
+                  '15- Gastos Operativos (Archivo)\n'
+                  '16- Eventos Especiales (Archivo)\n'
+                  '17- Inventario de Productos/Servicios (Archivo)\n'
+                  '18- Impacto de Factores Estacionales (Archivo)\n'
+                  '19- Competencia Local (Archivo)\n'
+                  '20- Tendencias de Mercado (Archivo)\n'),
 
               // Funcionalidades Clave
               sectionTitle('Funcionalidades Clave'),
               sectionContent(
-                  'La aplicación permite a los usuarios crear, leer, actualizar y eliminar información sobre los establecimientos '
+                  'La sección de establecimientos de la aplicación permite a los usuarios crear, leer, actualizar y eliminar información sobre los establecimientos '
                   'mediante una interfaz intuitiva, facilitando la gestión de los datos.'),
 
               // Interfaz de Usuario
-              sectionTitle('Interfaz de Usuario'),
+              sectionSubtitle('Interfaz de Usuario'),
               sectionContent(
                   'La aplicación incluye tarjetas y páginas detalladas para cada establecimiento, permitiendo a los usuarios '
                   'acceder a información completa y realizar acciones específicas relacionadas con cada establecimiento.'),
 
               // Integraciones y Servicios Externos
-              sectionTitle('Integraciones y Servicios Externos'),
+              sectionSubtitle('Integraciones y Servicios Externos'),
               sectionContent(
-                  'La aplicación utiliza servicios de mapas y geolocalización para presentar la ubicación exacta de los establecimientos, '
+                  'La aplicación utiliza servicios de mapas y geolocalización para manejar la ubicación exacta de los establecimientos, '
                   'mejorando la experiencia del usuario y proporcionando datos precisos para el análisis.'),
 
               // Seguridad y Privacidad
-              sectionTitle('Seguridad y Privacidad'),
+              sectionSubtitle('Seguridad y Privacidad'),
               sectionContent(
                   'Se implementan medidas rigurosas para proteger los datos sensibles de los establecimientos. Los accesos y permisos '
                   'se gestionan cuidadosamente para asegurar que solo los usuarios autorizados puedan ver o editar la información.'),
 
               // Casos de Uso y Ejemplos
-              sectionTitle('Casos de Uso y Ejemplos'),
+              sectionSubtitle('Casos de Uso y Ejemplos'),
               sectionContent(
                   'Los establecimientos del Grupo Ramos son utilizados en diversos escenarios dentro de la aplicación, '
                   'desde análisis de datos hasta interacciones específicas del usuario con cada establecimiento.'),
@@ -90,4 +114,16 @@ class InstitutionsWiki extends StatelessWidget {
       style: TextStyle(fontSize: 16),
     );
   }
+}
+
+// Método para Subtítulos
+Widget sectionSubtitle(String subtitle) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 8.0),
+    child: Text(
+      subtitle,
+      style: TextStyle(
+          fontSize: 18, fontWeight: FontWeight.w500, color: Colors.black54),
+    ),
+  );
 }
